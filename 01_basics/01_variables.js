ef201f2205b2410ce07da11935f50c44564e830a
Bhushan450 <bhushanmhaske45@gmail.com>
2025-06-19 08:15:00 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const account_id=254835
+let account_email="bhushan542google.com "
+var account_password="123345"
+account_City="jaipur"
+let account_state;
+
+// account_id=2   // not allowed
+
+account_email="bhushan785google.com"
+account_password="454554"
+account_City="pune"
+
+console.log(account_id);
+console.table([account_id,account_email,account_password,account_City,account_state])
+
+/*
+prefer not to use 'var'
+becas=use of isssue in block scope and functional scope
+*/
+
+
