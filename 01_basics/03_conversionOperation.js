0affec208ac2ed0106467a4ff0efdc7b8f6662c7
Bhushan450 <bhushanmhaske45@gmail.com>
2025-06-27 04:43:27 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score="33"      // score= null , undefined ,boolean values
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  // NaN=> not a number
+
+//"33"(convert into)=>33
+//"33abc"=>NaN
+// true=> 1 ; false=>0
+
+let LoggedIn=1
+
+let boolanIsLoggedIn=Boolean(LoggedIn)
+
+//console.log( typeof boolanIsLoggedIn);
+//console.log(boolanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber=45
+
+let strigNumber= String(someNumber)
+console.log(strigNumber);
+console.log(typeof strigNumber);
+
+
+
+
