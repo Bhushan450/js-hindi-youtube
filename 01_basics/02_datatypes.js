16b99e378af76111f14798615807c7e7e066152b
Bhushan450 <bhushanmhaske45@gmail.com>
2025-06-27 04:03:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as a newer version
+
+//alert(3+3)  // we arre using node js not browser
+
+console.log(3
+    +
+    3)
+// code readibility should be high
+
+console.log("bhushan")
+
+let name="bhushan"
+let number=45;
+let isLoggedIn=false
+
+//number=> 2 to the power 53
+//bigint
+//string=>""
+// boolean=> true or false
+// null=>standalone value
+// undefined
+//symbol=> unique
+
+// objects
+
+// typeof--use for knowing the type of data types
+
+console.log(typeof "bhushan");
+console.log(typeof number);
+console.log(typeof undefined); // O/P=>undefined
+console.log(typeof null); // O/P=> object
+
+
+
