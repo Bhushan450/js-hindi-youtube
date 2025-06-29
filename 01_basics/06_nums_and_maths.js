59f15356b9ea1a3b2de9b0898e9b0a4a6770c636
Bhushan450 <bhushanmhaske45@gmail.com>
2025-06-29 16:51:37 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,50 @@
+const score=400
+//console.log(score);
+
+
+const balance= new Number(100)
+//console.log(balance);
+
+//onsole.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const anotheNumber=123.8967
+// console.log(anotheNumber.toPrecision(3));
+// console.log(anotheNumber.toPrecision(4));
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++ Maths ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(4.5)); // roundof 
+// console.log(Math.ceil(4.1));  // thodi jari value varti geli tri uppar / higest value select karnar
+// console.log(Math.floor(4.9)); // lower value ch select karnar
+// console.log(Math.min(3,7,6,8)); // gives or detects the minimum value
+// console.log(Math.max(4,6,3,9)); // detects the maximum value
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() *(max-min+1) +min)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
