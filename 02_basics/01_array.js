3f83e6190e1ac0a6a19564c6644119b8ace2f790
Bhushan450 <bhushanmhaske45@gmail.com>
2025-07-01 18:03:52 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,47 @@
+// Array
+
+const myArray=[0,1,2,3,4,5]
+const myHero=["iron] man","thor","captain america"]
+
+const myArray2=new Array(1,2,3,4,5)
+// console.log(myArray[4]);
+
+// Array methods
+
+// myArray.push(6) // push=> to add the values in an array
+// myArray.push(7)
+// myArray.push(8)
+// myArray.pop()  // pop=>it removes the last element from an array and it does not require any argument 
+
+// myArray.unshift(5) // unshift=> it adds the value in the starting of the array
+// myArray.shift() // it removes the starting element from the array 
+
+// console.log(myArray.includes(6)); // asks a question to array
+// console.log(indexof(5));  // gives the index of 5
+
+// const newArr= myArray.join() // converts array elements into an string
+// console.log(myArray);
+// console.log(newArr);
+// console.log(typeof(newArr));
+
+console.log("A",myArray);
+
+const myn1=myArray.slice(1,3) // O/P=> 1,2
+console.log(myn1);
+console.log("B",myArray);
+
+const myn2=myArray.splice(1,3) // splice manipulates the main array
+console.log(myn2);
+console.log("c",myArray);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/02_array.js
@@ -0,0 +1,12 @@
+const marvel_Heros=["thor","iron man","spider man"]
+const dc_Heros=["superman","flash","batman"]
+
+marvel_Heros.push(dc_Heros)
+
+// console.log(marvel_Heros);
+// console.log(marvel_Heros[3][1]);
+
+const allHeros=marvel_Heros.concat(dc_Heros)
+console.log(allHeros);
+
+
