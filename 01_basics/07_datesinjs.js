25944a67605b26e0d8147d3fae8716a9cda253d7
Bhushan450 <bhushanmhaske45@gmail.com>
2025-07-01 16:31:35 +0000

    dates in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,18 @@
+// Dates
+
+const myDate=new Date()
+//console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+
+let mycreatedDate=new Date(2005,6,17)
+console.log(mycreatedDate);
+console.log(mycreatedDate.getDate());
+
+
