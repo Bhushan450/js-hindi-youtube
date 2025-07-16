c7df3e8c1bc8d9b59ad719b70374a3de82639ed2
Bhushan450 <bhushanmhaske45@gmail.com>
2025-07-16 17:48:52 +0000

    if-else conditions done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+// if
+const isLoggedIn=true
+const temp=41
+
+// if (temp<50) {
+//     console.log("temperatur is less than 50");
+   
+// }
+// else{
+//     console.log("temp is higher than 50");}
+
+// console.log("executed");
+
+
+// <,>,<=,>=,==,!=,===,!==
+
+// const user=200
+
+// if (user>100) {
+//     const power="fly"
+//     console.log(`power is ${power}`);
+//}
+// console.log(`power is ${power}`);   // block scope
+
+
+const balance=1000 
+
+//if (balance>500) console.log("test"),console.log("test2")
+
+// if (balance<500) {
+//     console.log("less than 500");  
+// }
+    
+// else if (balance<750) {
+//     console.log("less than 750");  
+// }
+    
+// else if (balance<900) {
+//     console.log("less than 900");  
+// }
+    
+// else {
+//     console.log("less than 1200");  
+// }
+
+
+const userloggedIn=true
+const debitCard=true
+const loggedInfrogoogle=true
+const loggedInfromEmail=true
+
+if(userloggedIn && debitCard && 2==2){
+    console.log("allow to shopping" );
+}
+
+if (loggedInfrogoogle || loggedInfromEmail) {
+    console.log("just logged in");
+    
+}
+    
