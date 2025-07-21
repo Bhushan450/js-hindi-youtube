3c1e1d2c655786dbf21d26a3f62fa9dac1d0929b
Bhushan450 <bhushanmhaske45@gmail.com>
2025-07-21 01:59:33 +0000

    for and do while loops done

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+
+let index=0
+
+while (index<=10) {
+    //console.log(`value of index is: ${index}`);
+     index=index+2  // if this condition is misses then code will be infinitely run 
+}
+
+
+let myarray=["bhushan","vaibhav","aditya"]
+
+let arr=0
+
+while (arr<myarray.length) {
+    //console.log(`values of array is: ${myarray[arr]}`);
+    arr++
+}
+
+let score=11
+
+do {
+    console.log(`score is: ${score}`);
+    score++
+
+} while (score<=10);
