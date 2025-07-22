28126c6d20588a1579a3917abf3a19c8df73f08c
Bhushan450 <bhushanmhaske45@gmail.com>
2025-07-22 13:15:13 +0000

    for-each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,51 @@
+const language=["js","ruby","py","cpp","py"]
+
+// ****** For-Each loop **********8
+
+language.forEach( function (item){         
+   //console.log(item);                //callback function la nav nasta
+}
+  )    
+
+language.forEach((value)=>{   // by using array function 
+   // console.log(value);
+}
+  )
+
+
+  function printMe(value){
+     console.log(value);
+     
+  }
+
+  //language.forEach(printMe)
+
+  language.forEach( (item,index,arr)=>{
+   // console.log(item,index,arr);
+    }
+ )
+
+
+ const mycoding=[
+   {
+     languageName:"javascript",
+     languageFile:"js",
+   },
+   {
+     languageName:"java",
+     languageFile:"java",
+   },
+   {
+     languageName:"python",
+     languageFile:"py",
+   },
+ ]
+
+ mycoding.forEach( (item)=>{
+    console.log(item.languageName);
+    
+   }
+)
+
+
+
